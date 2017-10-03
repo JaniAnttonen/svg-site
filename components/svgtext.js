36305b2bb0b66d5107600ea50894f7c27e2e601c
Jani Anttonen <jantto@utu.fi>
2017-10-04 00:02:21 +0300

    handle rows better by making separate paths instead of separate svgs

--- components/svgtext.js
+++ components/svgtext.js
@@ -1,27 +1,63 @@
 import React from 'react';
 import TextToSVG from 'text-to-svg';
 
+const splitText = (text, columns) => {
+  const isWhiteSpace = char => /\s/.test(char);
+  const charLimit = columns || 80;
+  let charInRow = 0;
+  let row = '';
+  const rows = [];
+  for (const char of text) {
+    row += char;
+    charInRow += 1;
+    if ((charInRow >= charLimit && isWhiteSpace(char)) || text.indexOf(char) === text.length) {
+      rows.push(row);
+      row = '';
+      charInRow = 0;
+    }
+  }
+  return rows;
+};
 
 export default class SVGText extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            element: null
-        }
-    }
+  constructor(props) {
+    super(props);
+    const textToSVG = TextToSVG.loadSync();
+    this.attributes = this.props.attributes || { fill: '#c0bdf0' };
+    const options = this.props.options || {
+      x: 0,
+      y: 0,
+      fontSize: 11,
+      anchor: 'top',
+    };
+    const rows = splitText(this.props.text);
+    const maxWidth = rows.reduce((a, b) => Math.max(textToSVG.getMetrics(a, options).width, textToSVG.getMetrics(b, options).width));
+    const totalHeight = rows.map((text) => textToSVG.getMetrics(text, options).height).reduce((a, b) => a + b + options.fontSize, 0);
+    this.state = {
+      paths: rows.map((row, index) => {
+        options.y = index > 0 ? options.y + options.fontSize * 2 : options.y;
+        return textToSVG.getD(row, options);
+      }),
+      svgWidth: maxWidth,
+      svgHeight: totalHeight,
+    };
+    this.pathElems = [];
+  }
 
-    componentWillMount() {
-        const textToSVG = TextToSVG.loadSync();
-        const attributes = this.props.attributes || {fill: '#c0bdf0'};
-        const options = this.props.options || {x: 0, y: 0, fontSize: 11, anchor: 'top', attributes: attributes};
-        this.setState({
-            element: textToSVG.getSVG(this.props.text || 'hello world!', options)
-        });
-    }
-
-    render() {
-        return this.state.element && (
-            <div dangerouslySetInnerHTML={{__html: this.state.element}}/>
-        );
-    }
+  render() {
+    return (
+      this.state.paths && (
+        <svg
+          ref={elem => (this.svg = elem)}
+          width={this.state.svgWidth}
+          height={this.state.svgHeight} 
+        >
+            {this.state.paths.map((path, index) => {
+                this.pathElems.push(`row${index}`);
+                return <path d={path} fill={this.attributes.fill} id={`row${index}`} key={`row${index}`} />
+            })}
+        </svg>
+      )
+    );
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,18 @@ import css from 'next/css';
 import SVGText from '../components/svgtext';
 
 const style = css({
-    width: 'auto',
-    maxWidth: '100%',
-    position: 'relative',
-    margin: 0
+  width: 'auto',
+  maxWidth: '100%',
+  position: 'relative',
+  margin: 0,
 });
 
-const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-const splitText = () => {
-    const isWhiteSpace = char => /\s/.test(char)
-    const charLimit = 80;
-    let charInRow = 0;
-    let row = '';
-    let rows = [];
-    for(let char of text) {
-        row += char;
-        charInRow += 1;
-        if((charInRow>=charLimit && isWhiteSpace(char)) || text.indexOf(char)===text.length) {
-            rows.push(row);
-            row = '';
-            charInRow = 0;
-        }
-    }
-    return rows;
-}
+const text =
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 export default () => (
-    <div className={style}>
-        <style>{`
+  <div className={style}>
+    <style>{`
           body { 
             min-height: 100vh;
             margin: 0;
@@ -42,8 +25,6 @@ export default () => (
             align-items: center;
           }
         `}</style>
-        {splitText().map((row, index) =>
-            <SVGText text={row} key={`row${index}`}/>
-        )} 
-    </div>
+    <SVGText text={text} />
+  </div>
 );
